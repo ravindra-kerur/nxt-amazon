bbbe3cfed49f65f2920727ced5ea6961d28ba59b
ravindra-kerur <ravispatil@hotmail.com>
2026-07-05 01:16:11 +0530

    Added Carouse to the landing page

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -6,14 +6,10 @@ const Menu = () => {
     <div className="flex justify-end">
       <div className="flex w-full gap-3">
         <Link href="/signin" className="flex items-center header-button">
-          {/* <UserIcon className="w-8 h-8" /> */}
-          {/* <span className="font-bold">Sign In</span> */}
           Hello, Sign in
         </Link>
 
         <Link href="/cart" className="header-button">
-          {/* <ShoppingCartIcon className="w-8 h-8" />
-          <span className="font-bold">Cart</span> */}
           <div className="flex items-end">
             <ShoppingCartIcon className="w-8 h-8" />
             Cart
