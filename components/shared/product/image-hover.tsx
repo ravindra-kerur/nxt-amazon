e0a2da39e1e8155c7564e855c0e6c941cacece2b
ravindra-kerur <ravispatil@hotmail.com>
2026-07-07 11:29:02 +0530

    Implementation of Todays deal section in Landing page

---
+++ components/shared/product/image-hover.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Image from "next/image";
+import React, { useRef, useState } from "react";
+
+const ImageHover = ({
+  src,
+  hoverSrc,
+  alt,
+}: {
+  src: string;
+  hoverSrc: string;
+  alt: string;
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  // let hoverTimeOut: any;
+
+  // const handleMouseEnter = () => {
+  //   hoverTimeOut = setTimeout(() => setIsHovered(true), 1000);
+  //   console.log('entered')
+  // };
+
+  // const handleMouseLeave = () => {
+  //   clearTimeout(hoverTimeOut);
+  //   console.log('left')
+  //   setIsHovered(false);
+  // };
+
+  const hoverTimeout = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const handleMouseEnter = () => {
+    hoverTimeout.current = setTimeout(() => {
+      setIsHovered(true);
+    }, 100);
+  };
+
+  const handleMouseLeave = () => {
+    if (hoverTimeout.current) {
+      clearTimeout(hoverTimeout.current);
+      hoverTimeout.current = null;
+    }
+
+    setIsHovered(false);
+  };
+
+  return (
+    <div
+      className="relative h-52"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        sizes="80vw"
+        className={`object-contain transition-opacity duration-500 
+            ${isHovered ? "opacity-0" : "opacity-100"}`}
+      />
+
+      <Image
+        src={hoverSrc}
+        alt={alt}
+        fill
+        sizes="80vw"
+        className={`absolute inset-0 object-contain transition-opacity duration-500
+            ${isHovered ? "opacity-100" : "opacity-0"}`}
+      />
+    </div>
+  );
+};
+
+export default ImageHover;
