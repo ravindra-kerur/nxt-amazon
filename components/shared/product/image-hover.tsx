166cb78331d36d0e7d6586c9bc01f3c87df37f36
ravindra-kerur <ravispatil@hotmail.com>
2026-07-07 11:56:28 +0530

    Implementation of Best selling section in Landing page

--- components/shared/product/image-hover.tsx
+++ components/shared/product/image-hover.tsx
@@ -29,7 +29,7 @@ const ImageHover = ({
   const handleMouseEnter = () => {
     hoverTimeout.current = setTimeout(() => {
       setIsHovered(true);
-    }, 100);
+    }, 1000);
   };
 
   const handleMouseLeave = () => {
