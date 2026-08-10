8c19bb231f1bab25561ca64b64628f54fdb2479d
ravindra-kerur <ravispatil@hotmail.com>
2026-08-10 23:43:09 +0530

    Fix for vercel build issue

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,19 +1,41 @@
-"use client";
-
-import { useSession } from "next-auth/react";
+import { auth } from "@/auth";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Checkout",
+  description: "Checkout page",
 };
 
-const CheckoutPage = () => {
-  const { data: session } = useSession();
+const CheckoutPage = async () => {
+  const session = await auth();
+
   if (!session?.user) {
     redirect("/sign-in?callbackUrl=/checkout");
   }
+
   return <div>Checkout form</div>;
 };
 
 export default CheckoutPage;
+
+// "use client";
+
+// import { useSession } from "next-auth/react";
+// import { Metadata } from "next";
+// import { redirect } from "next/navigation";
+
+// export const metadata: Metadata = {
+//   title: "Checkout",
+//   description: "Checkout page",
+// };
+
+// const CheckoutPage = () => {
+//   const { data: session } = useSession();
+//   if (!session?.user) {
+//     redirect("/sign-in?callbackUrl=/checkout");
+//   }
+//   return <div>Checkout form</div>;
+// };
+
+// export default CheckoutPage;
