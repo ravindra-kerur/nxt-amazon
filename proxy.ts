86cb17bca8ee726c55893f67f9f8db67876571bd
ravindra-kerur <ravispatil@hotmail.com>
2026-08-11 00:03:43 +0530

    Fix for build issue in proxy file

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,9 @@
-// import { NextRequest, NextResponse } from "next/server";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-export const { auth: proxy } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
 
-// export function proxy() {
-//   NextAuth(authConfig)
-// }
+export const proxy = auth;
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
