e9a8ab0510f9d82ce5bbb1651fb17b766f5914dc
ravindra-kerur <ravispatil@hotmail.com>
2026-08-10 23:45:43 +0530

    Fix for build issue

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -18,24 +18,3 @@ const CheckoutPage = async () => {
 };
 
 export default CheckoutPage;
-
-// "use client";
-
-// import { useSession } from "next-auth/react";
-// import { Metadata } from "next";
-// import { redirect } from "next/navigation";
-
-// export const metadata: Metadata = {
-//   title: "Checkout",
-//   description: "Checkout page",
-// };
-
-// const CheckoutPage = () => {
-//   const { data: session } = useSession();
-//   if (!session?.user) {
-//     redirect("/sign-in?callbackUrl=/checkout");
-//   }
-//   return <div>Checkout form</div>;
-// };
-
-// export default CheckoutPage;
