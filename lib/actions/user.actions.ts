606e42c54bc037e7508f3395116265e4a0a23605
ravindra-kerur <ravispatil@hotmail.com>
2026-08-18 20:03:21 +0530

    Implementation of Google Oauth provider

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -13,6 +13,12 @@ export const SignInWithCredentials = async (user: IUserSignIn) => {
   return await signIn("credentials", { ...user, redirect: false });
 };
 
+export const SignInWithGoogle = async () => {
+  await signIn("google", {
+    redirectTo: "/",
+  });
+};
+
 export const SignOut = async () => {
   const redirectTo = await signOut({ redirect: false });
   redirect(redirectTo.redirect);
